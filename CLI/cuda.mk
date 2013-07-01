53947f9875994d909fa47aa03110fd4d16e7cce3
Pedro Alves <pdroalves@gmail.com>
2013-07-01 00:41:17 -0300

    Bug fix. Agora modulos CUDA são compilados corretamente.

--- CLI/cuda.mk
+++ CLI/cuda.mk
@@ -1,7 +1,7 @@
 .cu.o:
-	$(NVCC) -gencode=arch=compute_13,code=sm_13 -o $@ -c $<
+	$(NVCC) -gencode=arch=compute_20,code=sm_20 -o $@ -c $<
 
 .cu.lo:
-	$(top_srcdir)/cudalt.py $@ $(NVCC) -gencode=arch=compute_13,code=sm_13 --compiler-options=\" $(CFLAGS) $(DEFAULT_INCLUDES) $(INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) \" -c $<
+	$(top_srcdir)/cudalt.py $@ $(NVCC) -gencode=arch=compute_20,code=sm_20 --compiler-options=\" $(CFLAGS) $(DEFAULT_INCLUDES) $(INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) \" -c $<
 
 
